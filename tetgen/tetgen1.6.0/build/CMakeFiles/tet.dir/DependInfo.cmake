866ccf4154c97223bca6709cc8b0348eecb672ce
lucaspayne <lucaspayyne@gmail.com>
2021-07-09 19:48:13 +1200

    Notes

---
+++ tetgen/tetgen1.6.0/build/CMakeFiles/tet.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucas/Continuum/tetgen/tetgen1.6.0/predicates.cxx" "CMakeFiles/tet.dir/predicates.cxx.o" "gcc" "CMakeFiles/tet.dir/predicates.cxx.o.d"
+  "/home/lucas/Continuum/tetgen/tetgen1.6.0/tetgen.cxx" "CMakeFiles/tet.dir/tetgen.cxx.o" "gcc" "CMakeFiles/tet.dir/tetgen.cxx.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tetgen/tetgen1.6.0/build/CMakeFiles/tet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tet.dir/predicates.cxx.o"
+  "CMakeFiles/tet.dir/predicates.cxx.o.d"
+  "CMakeFiles/tet.dir/tetgen.cxx.o"
+  "CMakeFiles/tet.dir/tetgen.cxx.o.d"
+  "libtet.a"
+  "libtet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
