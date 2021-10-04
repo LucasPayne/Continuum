a480a0a1261559ba76d1a03b21ab3443a175c22f
lucaspayne <lucaspayyne@gmail.com>
2021-10-05 12:45:46 +1300

    source term thing seems to work

---
+++ FiniteElements/build/CMakeFiles/FiniteVolumes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FiniteVolumes.dir/FiniteVolumes.cpp.o"
+  "CMakeFiles/FiniteVolumes.dir/FiniteVolumes.cpp.o.d"
+  "FiniteVolumes"
+  "FiniteVolumes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FiniteVolumes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
