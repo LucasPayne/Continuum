0b44b408f496b0d45a0011e1cd1a97049946e88e
lucaspayne <lucaspayyne@gmail.com>
2021-10-09 19:34:38 +1300

    tessellation shader for quadratic wireframe, need to make nicer

---
+++ FiniteElements/build/CMakeFiles/QuadraticLaplaceSolver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuadraticLaplaceSolver.dir/QuadraticLaplaceSolver.cpp.o"
+  "CMakeFiles/QuadraticLaplaceSolver.dir/QuadraticLaplaceSolver.cpp.o.d"
+  "QuadraticLaplaceSolver"
+  "QuadraticLaplaceSolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuadraticLaplaceSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FiniteElements/build/CMakeFiles/QuadraticLaplaceSolver.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for QuadraticLaplaceSolver.
+# This may be replaced when dependencies are built.
